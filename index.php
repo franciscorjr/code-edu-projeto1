10e87fccddb9c696c18dbddd364cdaf36cd03d55
Francisco Júnior <francisco_jr@outlook.com>
2017-03-03 06:04:10 -0400

    Adicionando Olá mundo usando PHP

---
+++ index.php
@@ -0,0 +1,8 @@
+<html lang="pt">
+    <head>
+        <title><?php echo "Olá Mundo em PHP "; ?></title>
+    </head>
+    <body>
+        <h1><?php echo "Olá Mundo, usando PHP!!!" ?></h1>
+    </body>
+</html>
